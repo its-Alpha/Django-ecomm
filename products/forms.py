da8890d0683a85709fc377559132912e49053b0f
its-Alpha <shahikriten08@gmail.com>
2022-02-28 20:36:41 +0400

    f commit

---
+++ products/forms.py
@@ -0,0 +1,26 @@
+from django.forms import ModelForm, fields
+from .models import *
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Orders
+        fields = ['quantity', 'payment_method', 'contact_no', 'address']
+
+
+class EditOrderForm(ModelForm):
+    class Meta:
+        model = Orders
+        fields = ['status', 'payment_status']
